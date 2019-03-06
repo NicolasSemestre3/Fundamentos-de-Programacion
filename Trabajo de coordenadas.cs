0125b5f8b7366e98872ffa7167908b9ebf591f13
Nicolás Escobar Espinosa <47011291+NicolasSemestre3@users.noreply.github.com>
2019-03-06 07:45:53 -0500

    Add files via upload

---
+++ Trabajo de coordenadas.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordenadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Pedimos informacion inicial
+            Console.WriteLine("Saludos soy Failsafe, hoy capitan verems si las coordenadas introducidas pertenecen al trayecto recto de nuestro viaje,... ya estoy viendo quien será el que extrelle el nuevo Exodo Negro.");
+
+            double[] coordenadaX = { 0, 0, 0, 0 };
+            double[] coordenadaY = { 0, 0, 0, 0 };
+
+            double[] m = { 0, 0, 0, 0 };
+            double[] b = { 0, 0, 0, 0 };
+            double[] distancias = { 0, 0, 0, 0 };
+
+            for (int contador = 0; contador < 3; contador++)
+            {
+                //pedimos los puntos
+                Console.WriteLine("\n\nCapitan dame las coordenadas de el lugar No" + (contador + 1) + " en X");
+                coordenadaX[contador] = double.Parse(Console.ReadLine());
+
+                //pedimos los puntos
+                Console.WriteLine("\nCapitan dame las coordenadas de el lugar No" + (contador + 1) + " en Y");
+                coordenadaY[contador] = double.Parse(Console.ReadLine());
+
+                //Narramos
+                Console.WriteLine("\n\nGrabando");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+            }
+
+            //calculamos
+
+            for (int contadorM = 0; contadorM < 2; contadorM++)
+            {
+                //Calculamos pendiente
+                m[contadorM] = (coordenadaY[contadorM + 1] - coordenadaY[contadorM]) / (coordenadaX[contadorM + 1] - coordenadaY[contadorM]);
+            }
+
+            for (int contadorB = 0; contadorB < 2; contadorB++)
+            {
+                //Calculamos b
+                b[contadorB] = coordenadaY[contadorB] - (m[contadorB] * coordenadaX[contadorB]);
+            }
+
+            //clasificamos
+            for (int contadorRecta = 0; contadorRecta < 2; contadorRecta++)
+            {
+                if (m[contadorRecta] == m[contadorRecta + 1] && b[contadorRecta] == b[contadorRecta + 1])
+                {
+                    Console.WriteLine("\n\nEl punto (" + coordenadaX[contadorRecta] + " , " + coordenadaY[contadorRecta] + ") y el punto (" + coordenadaX[contadorRecta + 1] + " , " + coordenadaY[contadorRecta + 1] + ") Estan en la misma recta");
+                }
+                else
+                {
+                    Console.WriteLine("\n\nEl punto (" + coordenadaX[contadorRecta] + " , " + coordenadaY[contadorRecta] + ") y el punto (" + coordenadaX[contadorRecta + 1] + " , " + coordenadaY[contadorRecta + 1] + ") Estan en una diferente recta");
+                }
+            }
+
+            //distancia
+            double mayorDistancia = 0;
+            double puntoInicialX = 0;
+            double puntoFinalX = 0;
+            double puntoInicialY = 0;
+            double puntoFinalY = 0;
+
+            for (int contadorD = 0; contadorD < 2; contadorD++)
+            {
+                distancias[contadorD] = Math.Sqrt(Math.Pow((coordenadaX[contadorD] - coordenadaX[contadorD + 1]), 2) + Math.Pow((coordenadaY[contadorD] - coordenadaY[contadorD + 1]), 2));
+
+                if (distancias[contadorD] > mayorDistancia)
+                {
+                    mayorDistancia = distancias[contadorD];
+                    puntoInicialX = coordenadaX[contadorD];
+                    puntoFinalX = coordenadaX[contadorD + 1];
+                    puntoInicialY = coordenadaY[contadorD];
+                    puntoFinalY = coordenadaY[contadorD + 1];
+                }
+            }
+
+            Console.WriteLine("\n\nLa mayor distancia fue : " + mayorDistancia);
+            Console.WriteLine("Entre el punto (" + puntoInicialX + " , " + puntoInicialY + ") y el punto (" + puntoFinalX + " , " + puntoFinalY + " )" );
+
+        }
+    }
+}
---
+++ Trabajo de edades.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordenadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Pedimos informacion inicial
+            Console.WriteLine("Saludos soy Failsafe, hoy capitan tendremos una fiesta, de no muchos invitados, dame sus nombres y edades..., no es como si fuesemos a hacer algo muy loco :V....");
+            Console.WriteLine("\nDime cuandos vendran...");
+            int tamaño = int.Parse(Console.ReadLine());
+
+            int[] edades = new int [tamaño];
+            string[] nombres = new string [tamaño];
+
+            int edadMayor = 0;
+            int edadMenor = 99;
+            string nombreMayor = "X";
+            string nombreMenor;
+
+            double totalEdades = 0;
+            double totalDesviable = 0;
+
+            double diferenciaEdades = 99;
+            string nombreCercaPromedio = "X";
+
+
+
+            for (int contador = 0; contador < tamaño; contador++)
+            {
+                //pedimos los nombres
+                Console.WriteLine("\n\nCapitan dame el nombre de el " + (contador + 1) + "° invitado ");
+                nombres[contador] = Console.ReadLine();
+
+                //pedimos las edades
+                Console.WriteLine("\nCapitan dame la edad de " + nombres[contador]);
+                edades[contador] = int.Parse(Console.ReadLine());
+
+                //Calculamos
+                totalEdades += edades[contador];
+
+
+                //Narramos
+                Console.WriteLine("\n\nGrabando");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+
+                //grabamos al mayor
+                if (edades[contador] > edadMayor)
+                {
+                    edadMayor = edades[contador];
+                    nombreMayor = nombres[contador];
+                }
+
+                //Grabamos al menor
+                if (edades[contador] < edadMenor )
+                {
+                    edadMenor = edades[contador];
+                    nombreMenor = nombres[contador];
+ 
+                }
+            }
+
+            //Enunciamos al mayor y menor
+            Console.WriteLine("\n\nDe tus invitados el mayor fue: " + nombreMayor + " con la edad de: " + edadMayor);
+            Console.WriteLine("\nDe tus invitados el menor fue: " + nombreMayor + " con la edad de: " + edadMenor);
+
+            //Promediamos
+            double promedio = totalEdades / tamaño;
+
+            for(int contadorDiferencias = 0; contadorDiferencias < tamaño; contadorDiferencias++)
+            {
+                if (Math.Abs(edades[contadorDiferencias] - promedio) < diferenciaEdades)
+                {
+                    diferenciaEdades = Math.Abs(edades[contadorDiferencias] - promedio);
+                    nombreCercaPromedio = nombres[contadorDiferencias];
+                }
+            }
+
+            //Enunciamos
+            Console.WriteLine("\n\nEl promedio de edades fue: " + promedio + " y la persona con la edad mas cercana a esta es... " + nombreCercaPromedio);
+
+            //desviamos
+            for (int contadorDesviable = 0; contadorDesviable < tamaño ; contadorDesviable++)
+            {
+                totalDesviable += Math.Pow((promedio - edades[contadorDesviable]), 2);
+            }
+
+            double desviacion = Math.Sqrt(totalDesviable / tamaño);
+
+            Console.WriteLine("\nLa desviacion fue de: " + desviacion);
+        }
+    }
+}
